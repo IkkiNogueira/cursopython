3f766f6d44b1764c223890dc91a2a5ddaa75b4f8
IkkiNogueira <ph-nogueira@hotmail.com>
2019-09-30 22:05:58 -0300

    mais coisinhas

--- aula1.py
+++ aula1.py
@@ -9,6 +9,11 @@ subtração = 1 - 3
 multiplicacao = 7 * 8
 exponenciacao = 7**8
 
+nome = input("Digite o seu nome: ")
+frase = 'Olá, {}'.format(nome)
+
+soma2 = '{} x {} = {}'.format(7,6,7*6)
+
 print(soma)
 print(subtração)
 print(multiplicacao)
@@ -16,3 +21,6 @@ print(exponenciacao)
 print(x)
 print(x+y)
 print(z)
+print(nome)
+print(frase)
+print(soma2)
