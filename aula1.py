37e73ec2f798aeb15b9e06ae53f7b4a5152dee60
IkkiNogueira <ph-nogueira@hotmail.com>
2019-09-30 21:39:53 -0300

    mais codigo

--- aula1.py
+++ aula1.py
@@ -1,4 +1,18 @@
-a = 1
-var = "Testando python na aula:"
+import math
 
-print(var, a)
+x = math.sqrt(16)
+y = 10
+z = x+y
+
+soma = 2.2 + 2.7
+subtração = 1 - 3
+multiplicacao = 7 * 8
+exponenciacao = 7**8
+
+print(soma)
+print(subtração)
+print(multiplicacao)
+print(exponenciacao)
+print(x)
+print(x+y)
+print(z)
